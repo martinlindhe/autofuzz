efacf6c6997af12c617da9cd687acbb6f5e77e97
Martin Lindhe <martin@ubique.se>
2014-10-07 22:50:35 +0200

    fix up class

--- doctor.py
+++ doctor.py
@@ -3,13 +3,22 @@ import subprocess
 import sys
 import psutil
 
-from distutils.version import LooseVersion, StrictVersion
+from distutils.version import StrictVersion
 
-# TODO move OsProbe to class lib & auto load it
+# TODO move OsxProbe to class lib & auto load it
 # TODO detect installed Xcode version
 
 
 class OsxProbe:
+    def RunCommand(self, command):
+        return subprocess.check_output(
+            command,
+            shell=True,
+            stderr=subprocess.STDOUT)
+
+    def GetVersionNumber(self):
+        raw = self.RunCommand("sw_vers -productVersion")
+        return raw.decode('ascii').strip("\n")
 
     def KillProcessAndChildren(proc_pid):
         # TODO move to util class
@@ -19,32 +28,26 @@ class OsxProbe:
         process.kill()
         return
 
-    def GetOsxVersionNumber():
-        proc = subprocess.Popen("sw_vers -productVersion", shell=True, universal_newlines=True, stdout=subprocess.PIPE)
-        res = proc.stdout.read().strip("\n")
-        OsxProbe.KillProcessAndChildren(proc.pid)
-        return res
-
-    def Is64Bit():
+    def Is64Bit(self):
         if os.uname().machine == "x86_64":
             return True
         return False
 
-    def IsSupported():
-        ver = GetOsxVersionNumber()
+    def IsSupported(self):
+        ver = self.GetVersionNumber()
         if StrictVersion(ver) >= StrictVersion("10.9.5"):
-            if isDetected() and is64Bits():
+            if self.IsDetected() and self.Is64Bit():
                 return True
         return False
 
-    def IsDetected():
+    def IsDetected(self):
         if sys.platform == "darwin":
             return True
         return False
 
-    def GetOsxVersionName():
+    def GetVersionName(self):
         ''' @return eg "Mavericks" '''
-        ver = GetOsxVersionNumber()
+        ver = self.GetOsxVersionNumber()
 
         if StrictVersion(ver) >= StrictVersion("10.10.0"):
             return "Yosemite"
@@ -63,20 +66,21 @@ class OsxProbe:
 
         return "Unrecognized"
 
+probe = OsxProbe()
 
-if not OsxProbe.IsDetected():
+if not probe.IsDetected():
     print("ERROR - only OSX is supported")
     sys.exit()
 
-if OsxProbe.GetOsxVersionNumber() != "10.9.5":
+if probe.GetVersionNumber() != "10.9.5":
     # TODO accept multiple supported OSX versions
     print("ERROR - only OSX 10.9.5 is supported")
     sys.exit()
 
-if not OsxProbe.Is64Bit():
+if not probe.Is64Bit():
     print("ERROR - not 64bit")
     sys.exit()
 
-if not OsxProbe.IsSupported():
+if not probe.IsSupported():
     print("ERROR - unsupported system")
     sys.exit()
