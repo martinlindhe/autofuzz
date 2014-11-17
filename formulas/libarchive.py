c8f17d70ddf3aa96e0abba4a91dd48cb2fe74474
Martin Lindhe <martin@ubique.se>
2014-11-17 16:28:50 +0100

    add (broken) formulas for bzip2, libarchive

---
+++ formulas/libarchive.py
@@ -0,0 +1,24 @@
+# STATUS broken, minitar dont compile
+
+class libarchive:
+    name = __name__
+    home = "http://libarchive.org/"
+    scmOrigin = "git clone https://github.com/libarchive/libarchive"
+    dataTypes = [
+        "deflate"  # XXXX
+    ]
+
+    target = "examples/minitar/minitar"
+    targetParam = ""
+    aflFuzzParam = ""
+
+    clean = [
+        "make distclean"
+    ]
+
+    build = [
+        "build/autogen.sh",
+        "CC=afl-gcc ./configure --disable-shared",
+        "make",
+        "afl-gcc -I libarchive -o examples/minitar/minitar examples/minitar/minitar.c .libs/libarchive.a -lz -lbz2"  # XXX dont compile
+    ]
