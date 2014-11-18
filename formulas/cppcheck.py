349fb1963d9a13070088a2c5150516d844a41581
Martin Lindhe <martin@ubique.se>
2014-11-18 19:33:15 +0100

    results: dropped cppcheck crash, turned out it was fixed in git master yesterday/today

--- formulas/cppcheck.py
+++ formulas/cppcheck.py
@@ -1,5 +1,7 @@
 # STATUS works, but is very slow (7/sec)
 
+# NOTE: for 25 hours it did not complete a run, but found 5171 paths
+
 class cppcheck:
     name = __name__
     home = "http://cppcheck.sourceforge.net/"
