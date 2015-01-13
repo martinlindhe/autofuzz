bbe87f8031aca10d90ef3bf99e251296ded3df96
Martin Lindhe <martin@ubique.se>
2015-01-13 23:16:08 +0100

    add udis86 formula

---
+++ formulas/udis86.py
@@ -0,0 +1,24 @@
+# STATUS runs
+
+
+class udis86:
+    name = __name__
+    home = "http://udis86.sourceforge.net/"
+    scmOrigin = "git clone https://github.com/vmt/udis86.git {destination}"
+    dataTypes = [
+        "elf"
+    ]
+
+    target = "udcli/udcli"
+    targetParam = ""
+    aflFuzzParam = ""
+
+    clean = [
+        "make distclean"
+    ]
+
+    build = [
+        "./autogen.sh",
+        "./configure --disable-shared CC=afl-gcc",
+        "make"
+    ]
